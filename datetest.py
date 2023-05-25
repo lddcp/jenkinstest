1509dc68a65b04fa74287ec24d4f5ef271b16a52
leandro <ducamp.leandre1@gmail.com>
2023-05-25 16:16:02 +0200

    commit 1 - hehehe

---
+++ datetest.py
@@ -0,0 +1,9 @@
+from datetime import now
+datetimenow = datetime.now()
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+print("Date : " + mm + "/" + dd + "/" + yyyy +"/"+" " +hour+" :"+mi+" :"+ss)
